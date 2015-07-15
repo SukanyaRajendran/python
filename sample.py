ea6cad4217a6383b19de007c0ddd1eed652791ca
SukanyaRajendran <suku.3112@gmail.com>
2015-07-15 17:11:05 +0530

    first python code

---
+++ sample.py
@@ -0,0 +1,41 @@
+a=6
+print("a")
+print(a)
+a=10
+b=5
+c=7
+print(a ,b,c)
+
+str="hello world hi"
+print (str)
+print(str[0])
+print(str[2:5])
+print(str[2:])
+print (str *2)
+print (str + "abc")
+
+list=['abc',78,34,'facts']
+print(list)
+print(list[2])
+print(list[1:3])
+print(list*2)
+
+tuple=('suk',6,9,'hog')
+print(tuple)
+print(tuple[2:4])
+
+dict={'name':'factspan','code':56,'age':8,'sales':'hike'}
+print(dict)
+print(dict.name())
+
+#a=45
+#if (a==45):
+#print("great")
+
+if(x<0):
+    x=0
+    print('negative')
+    elif x>1:
+    print('positive')
+    else:
+        print('more')
